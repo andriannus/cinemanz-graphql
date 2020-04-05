e09d34476b66202cca27c358b160dc1c6f0e1994
Andriannus Parasian <andriannus.parasian@amarbank.co.id>
2020-04-05 21:43:58 +0700

    feat: create input for movie data

--- graphql/type-defs/types/movie.type.js
+++ graphql/type-defs/types/movie.type.js
@@ -1,6 +1,22 @@
 const { gql } = require('apollo-server-express');
 
 const movieType = gql`
+  input DataMovie {
+    genre: [String]
+    minutes: Int
+    synopsis: String
+    producer: [String]
+    production: String
+    director: String
+    writer: String
+    cast: [String]
+    start: String
+    end: String
+    rate: Float
+    theater: [String]
+    poster: String
+  }
+
   type Movie {
     _id: ID
     title: String
